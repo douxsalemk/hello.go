4b811f397d27f774a91d4dbe85d2ff2450a11520
Doux Salem kayembe <76118979+douxsalemk@users.noreply.github.com>
2022-08-11 17:32:21 -0300

    feat: use function in go

--- hello.go
+++ hello.go
@@ -6,27 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Olá Mundo")
-	name := "Doux"
-	var versao float32 = 1.1
-	var tipo = 1.001
 
-	fmt.Println("O meu nome é: ", name)
-	fmt.Println("Este programa está na versao: ", versao)
-	fmt.Println("O tipo da variável é: ", reflect.TypeOf(tipo))
+	exibeIntroducao()
+	exibeMenu()
 
-	fmt.Println("\n \n ")
-
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do programa")
-
-	fmt.Println("\n  \n ")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O endereco da minha variavel comando é: ", &comando)
-	fmt.Println("O comando escolhido foi: ", comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -37,6 +21,34 @@ func main() {
 		fmt.Println("Saindo do programa...")
 	default:
 		fmt.Println("Não conheço este comando")
-	}
 
+	}
+}
+func exibeIntroducao() {
+	fmt.Println("Olá Mundo")
+	name := "Doux"
+	var versao float32 = 1.1
+	var tipo = 1.001
+
+	fmt.Println("O meu nome é: ", name)
+	fmt.Println("Este programa está na versao: ", versao)
+	fmt.Println("O tipo da variável é: ", reflect.TypeOf(tipo))
+
+	fmt.Println("\n \n ")
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+	fmt.Println("\n  \n ")
+
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variável comando é:", comandoLido)
+
+	return comandoLido
 }
