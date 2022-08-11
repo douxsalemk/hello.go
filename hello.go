25392912e19022a17621e5de71f829124ae14be8
Doux Salem kayembe <76118979+douxsalemk@users.noreply.github.com>
2022-08-11 17:43:36 -0300

    feat: monitoramento do site

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"reflect"
 )
@@ -16,6 +17,7 @@ func main() {
 	switch comando {
 	case 1:
 		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -36,14 +38,14 @@ func exibeIntroducao() {
 	fmt.Println("Este programa está na versao: ", versao)
 	fmt.Println("O tipo da variável é: ", reflect.TypeOf(tipo))
 
-	fmt.Println("\n \n ")
+	fmt.Println("\n ")
 }
 
 func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
-	fmt.Println("\n  \n ")
+	fmt.Println("\n ")
 
 }
 
@@ -53,4 +55,16 @@ func leComando() int {
 	fmt.Println("O valor da variável comando é:", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
